b125226491454c6eb7bc64d0dd0743af40fa3187
onjegv <vgejno@student.42lausanne.ch>
2023-06-25 21:14:40 +0200

    header

---
+++ ex02/main.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vgejno <vgejno@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/25 21:14:20 by vgejno            #+#    #+#             */
+/*   Updated: 2023/06/25 21:14:21 by vgejno           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+int main() {
+
+	std::string string = "HI THIS IS BRAIN";
+
+	std::string *stringPTR = &string;
+	std::string &stringREF = string;
+
+	std::cout << "Address of the string variable string: " << &string << std::endl;
+	std::cout << "Address held by stringPTR: " << stringPTR << std::endl;
+	std::cout << "Address held by stringREF: " << &stringREF << std::endl << std::endl;
+
+	std::cout << "Value of the string variable string: " << string << std::endl;
+	std::cout << "Value pointed to by stringPTR: " << *stringPTR << std::endl;
+	std::cout << "Value pointed to by stringREF: " << stringREF << std::endl;
+
+	return 0;
+}
